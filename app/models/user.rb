65d7d47090ea621d8ffdd05f99d7a81d51c02163
John Allen <john_allen3@student.uml.edu>
2016-05-20 16:50:48 -0400

    adding in some devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ActiveRecord::Base
-  enum role: %w(user it_chair assistant_it_chair)
+  enum role: %w(user admin)
   enum pc_position: %w(consul proconsul annotator scribe sergeant_at_arms custodian)
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :first_name, presence: :true
   validates :last_name, presence: :true
-  validates :role, presence: :true
-  validates :beta_number, presence: :true
 end
