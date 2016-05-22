ffd4eb67e78ea48309b2fabbe03cd4b00834ece1
John Allen <john_allen3@student.uml.edu>
2016-05-22 17:04:11 -0400

    working users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,4 @@ class User < ActiveRecord::Base
   enum pc_position: %w(consul proconsul annotator scribe sergeant_at_arms custodian)
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  validates :first_name, presence: :true
-  validates :last_name, presence: :true
 end
